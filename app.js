5f57bb744a3785a4bc86fe9339cc311ecabbbfaa
yanyanyaa <yclin3411@gmail.com>
2024-05-19 00:42:01 +0800

    feat: add todo seeder

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
+
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
+const PORT = process.env.PORT
 const app = express()
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -17,13 +20,15 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
 app.get('/', (req,res) => {
-  res.render('index')
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.log(error))
 })
 
 app.listen(PORT, () => {
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -6,14 +6,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < 0; i++) {
+  for (let i = 0; i < 10; i++) {
     Todo.create({name:`name-${i}`})
   }
   console.log('done')
