859060d8a0719fc07260c3b66e33c9044fd20a9a
yanyanyaa <yclin3411@gmail.com>
2024-05-18 23:30:29 +0800

    feat: add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < 0; i++) {
+    Todo.create({name:`name-${i}`})
+  }
+  console.log('done')
+})
