51078eae4e6abb4a44a9a79e2d2a0c0c6c3ebc9a
yanyanyaa <yclin3411@gmail.com>
2024-05-19 01:14:12 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -44,6 +44,14 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)
 })
