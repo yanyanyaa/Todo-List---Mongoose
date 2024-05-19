87653960c33ca0b8e57c39bca503908923c9dca8
yanyanyaa <yclin3411@gmail.com>
2024-05-19 08:11:52 +0800

    feat: add sort funciton

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos }))
     .catch(error => console.log(error))
 })
